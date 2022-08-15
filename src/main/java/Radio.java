7fad6f847f34e7ce6cc6c6dae98000a81fa216bb
Mikhail9845 <mihail007.08@mail.ru>
2022-08-15 22:41:13 +0300

    Added constructor

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,12 +2,43 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
     private int maxStation;
-    public Radio (){
+
+    public Radio() {
         maxStation = 9;
     }
-    public Radio (int stationCount){
+
+    public Radio(int stationCount) {
         maxStation = stationCount - 1;
     }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
 
     public void next() {
         if (currentStation < maxStation) {
@@ -38,31 +69,5 @@ public class Radio {
         }
     }
 
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > maxStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
 }
